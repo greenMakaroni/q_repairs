9a2f69a067378697f57e67c93b2b371c4a8dbb5f
greenMakaroni <mrtn.dawid@gmail.com>
2023-09-12 14:43:04 +0100

    install setup and configure firebase, build website and deploy to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+/* Firebase */
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 /* Router & Navigation*/
 import { RouterProvider} from "react-router-dom"
 import router from "./router.jsx"
+
+// config firebase
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
+}
+
+// init firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
 
